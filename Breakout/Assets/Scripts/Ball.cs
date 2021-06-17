e2ad00abbb094eb298b1a69d8784e4816383c6ae
Daniel-Taufiq <taufiqdanny@gmail.com>
2021-06-17 16:48:14 -0600

    basic game setup

---
+++ Breakout/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float speed = 20f;
+    Rigidbody rigidbody;
+    Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();  // have access to RigidBody component on Ball object
+        rigidbody.velocity = Vector3.down * speed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // shrink the current velocity to one unit and multiple by speed
+        rigidbody.velocity = rigidbody.velocity.normalized * speed;
+        velocity = rigidbody.velocity;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // ball will reflect off the normal plane from the Paddle object
+        rigidbody.velocity = Vector3.Reflect(velocity, collision.contacts[0].normal);
+    }
+}
---
+++ Breakout/Assets/Scripts/Brick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int hits = 1;
+    public int points = 100; 
+    public Vector3 rotator;
+    public Material hitMaterial;
+    Material originalMaterial;
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(rotator * (transform.position.x + transform.position.y) * 0.1f);
+        renderer = GetComponent<Renderer>();
+        originalMaterial = renderer.sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotator * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        hits--;
+        if (hits <= 0)
+        {
+            Destroy(gameObject);
+        }
+        renderer.sharedMaterial = hitMaterial;
+        Invoke("RestoreMaterial", 0.05f);
+    }
+
+    void RestoreMaterial()
+    {
+        renderer.sharedMaterial = originalMaterial;
+    }
+}
